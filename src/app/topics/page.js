0233fe6ea194496dddec282cea65b4913388da16
Alex <alexandriaduell@gmail.com>
2025-10-22 13:42:35 -0400

    Added Topics heading + started on the title/link component that will be mapped through

--- src/app/topics/page.js
+++ src/app/topics/page.js
@@ -2,6 +2,25 @@
 
 import { useRouter } from "next/navigation";
 
+const paths = {
+  addition: {
+    symbol: '',
+  }
+}
+
+
+const PathComponent = (props) => {
+  const { symbol, title } = props
+
+  return (
+    <div 
+      className="w-full"
+    >
+
+    </div>
+  )
+}
+
 
 export default function Topics() {
   const router = useRouter()
@@ -10,7 +29,17 @@ export default function Topics() {
     <div
       className="w-full mt-[32px] flex flex-wrap place-content-center"
     >
-        Topics Page
+      <h1
+        className="w-full text-[77px] text-white font-bold text-center"
+      >
+        Math Paths
+      </h1>
+
+      <div
+        className="w-full"
+      >
+
+      </div>
     </div>
   );
 }
