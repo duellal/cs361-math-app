dccf8147253b5e0abefa99de8e941a301001343c
Alex <alexandriaduell@gmail.com>
2025-12-01 21:16:45 -0500

    Made number inputs to sliders; modified POST call range based on number type + operands per problem; added disable to generate problems button

--- src/app/topics/addition/generate-problems/page.js
+++ src/app/topics/addition/generate-problems/page.js
@@ -2,9 +2,10 @@
 
 import generate_problem_set from '@/app/_apiFuncs/problems/addition/addProblemSet'
 import Link from 'next/link'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import easy_problems from '../easy/easyAdditionPrblms'
 import UserContext from '@/app/_context/userContext'
+import { useRouter } from 'next/navigation'
 
 // Styling:
 // Divs
@@ -16,9 +17,11 @@ const formFieldset =
 const formLegend = 'text-dark-blue p-2 font-bold text-xl'
 const formInput = 'border p-3 mb-2 w-[65%] text-white text-center'
 const formBtn =
-    'w-[55%] bg-dark-blue text-[18px] text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition'
+    'w-[55%] bg-dark-blue text-[18px] text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition cursor-pointer disabled:cursor-not-allowed disabled:opacity-[70%] mt-[20px]'
 
 export default function GenerateAddtionProblems() {
+    const router = useRouter()
+
     const numberTypeArr = ['select type', 'integer', 'decimal', 'fraction']
     const roundToArr = [
         'select place value',
@@ -32,31 +35,52 @@ export default function GenerateAddtionProblems() {
     const { user } = useContext(UserContext)
 
     const [error, setError] = useState(false)
-    const [prblm_count, setNumOfProbs] = useState('')
-    const [num_per_prblm, setNumofOperands] = useState('')
+    const [disableSubmit, setDisableSubmit] = useState(true)
+    const [prblm_count, setNumOfProbs] = useState(1)
+    const [num_per_prblm, setNumofOperands] = useState(2)
     const [format, setFormat] = useState('')
     const [round_to, setRoundTo] = useState('')
 
+    const prblm_count_range = Array.from({ length: 99 }, (__, idx) => idx + 1)
+    const num_per_prblm_range = Array.from({ length: 9 }, (__, idx) => idx + 2)
+
+    useEffect(() => {
+        if (
+            typeof prblm_count === 'number' &&
+            typeof num_per_prblm === 'number' &&
+            format !== '' &&
+            (format === 'decimal' ? typeof round_to === 'number' : true)
+        ) {
+            setDisableSubmit(false)
+        } else {
+            setDisableSubmit(true)
+        }
+    }, [format, num_per_prblm, prblm_count, round_to])
+
     const handleGenerateProblems = async (evt) => {
-        console.log('Handle Generate Problems?')
         evt.preventDefault()
+        let range = [0, 100 / num_per_prblm]
+
+        if (format === 'decimal') {
+            range = [0, 10 / num_per_prblm]
+        }
 
         let generateSet = await generate_problem_set({
             prblm_count,
             num_per_prblm,
             format,
             round_to,
-            range: [0, 100],
+            range,
             difficulty: 'easy',
         })
 
-        console.log('Generated set:', generateSet)
-
         if (generateSet?.status === 200) {
             await easy_problems({ user_id: user ? user['_id'] : null })
-            setError(false)
 
-            return <Link href={'/topics/addition/easy'} className={`w-[55%]`} />
+            setError(false)
+            setDisableSubmit(true)
+
+            return router.push('/topics/addition/easy')
         }
 
         setError(true)
@@ -88,6 +112,12 @@ export default function GenerateAddtionProblems() {
                             Fill out the form and click on submit to generate
                             more <strong>easy addition</strong> problems!
                         </p>
+                        <p className="w-[60%] text-center">
+                            <strong>Tip:</strong> You can move the slider with
+                            your keyboard! Click it (or press the Tab key to
+                            highlight it), then use the left and right arrow
+                            keys.
+                        </p>
                     </div>
                 </div>
 
@@ -100,24 +130,54 @@ export default function GenerateAddtionProblems() {
                         key={'fieldset-num-of-probs'}
                         className={`border-3 ${formFieldset}`}
                     >
-                        <legend className={formLegend}>
+                        <legend className={formLegend} htmlFor="num_of_probs">
                             How many more problems would you like?
                         </legend>
 
-                        <input
-                            type="number"
-                            name="num_of_probs"
-                            value={prblm_count}
-                            max={99}
-                            min={1}
-                            required
-                            placeholder="Enter a number between 1 - 99"
-                            className={formInput}
-                            onChange={(evt) => {
-                                evt.preventDefault()
-                                setNumOfProbs(Number(evt.target.value))
-                            }}
-                        />
+                        <div className="w-[90%] flex flex-wrap place-self-center">
+                            <div className="w-full text-xl font-semibold text-dark-blue">
+                                {prblm_count}
+                            </div>
+
+                            <input
+                                type="range"
+                                name="num_of_probs"
+                                id="num_of_probs"
+                                list="num_of_probs_markers"
+                                value={prblm_count}
+                                max={99}
+                                min={1}
+                                step={1}
+                                className={`appearance-none accent-dark-blue w-full ${formInput} px-0 py-0 my-[10px] text-black rounded-full bg-light-blue cursor-pointer`}
+                                onChange={(evt) => {
+                                    evt.preventDefault()
+                                    setNumOfProbs(Number(evt.target.value))
+                                }}
+                            />
+
+                            <div
+                                id="num_of_probs_markers"
+                                className="w-full flex justify-between ml-1 mt-[-5px] mb-[10px]"
+                            >
+                                {prblm_count_range.map((num, idx) => {
+                                    if (
+                                        idx === 0 ||
+                                        num % 10 === 0 ||
+                                        idx === prblm_count_range.length - 1
+                                    ) {
+                                        return (
+                                            <div
+                                                key={`${idx}-range-num-${num}`}
+                                            >
+                                                <span className="text-sm font-bold">
+                                                    {num}
+                                                </span>
+                                            </div>
+                                        )
+                                    }
+                                })}
+                            </div>
+                        </div>
                     </fieldset>
 
                     {/* Number of Operands */}
@@ -134,20 +194,42 @@ export default function GenerateAddtionProblems() {
                             </p>
                         </legend>
 
-                        <input
-                            type="number"
-                            name="num_of_probs"
-                            value={num_per_prblm}
-                            max={10}
-                            min={2}
-                            required
-                            placeholder="Enter a number between 2 - 10"
-                            className={formInput}
-                            onChange={(evt) => {
-                                evt.preventDefault()
-                                setNumofOperands(Number(evt.target.value))
-                            }}
-                        />
+                        <div className="w-[90%] flex flex-wrap place-self-center">
+                            <div className="w-full text-xl font-semibold text-dark-blue">
+                                {num_per_prblm}
+                            </div>
+
+                            <input
+                                type="range"
+                                name="num_of_probs"
+                                value={num_per_prblm}
+                                max={10}
+                                min={2}
+                                step={1}
+                                required
+                                placeholder="Enter a number between 2 - 10"
+                                className={`appearance-none accent-dark-blue w-full ${formInput} px-0 py-0 my-[10px] text-black rounded-full bg-light-blue cursor-pointer`}
+                                onChange={(evt) => {
+                                    evt.preventDefault()
+                                    setNumofOperands(Number(evt.target.value))
+                                }}
+                            />
+
+                            <div
+                                id="num_per_prob_markers"
+                                className="w-full flex justify-between ml-1 mt-[-5px] mb-[10px]"
+                            >
+                                {num_per_prblm_range.map((num, idx) => {
+                                    return (
+                                        <div key={`${idx}-range-num-${num}`}>
+                                            <span className="text-sm font-bold">
+                                                {num}
+                                            </span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </fieldset>
 
                     {/* Problem Format */}
@@ -162,7 +244,7 @@ export default function GenerateAddtionProblems() {
 
                         <select
                             name="number-type"
-                            className={`${formInput} capitalize`}
+                            className={`${formInput} text-dark-blue capitalize`}
                             required
                             onChange={(evt) => {
                                 evt.preventDefault()
@@ -212,7 +294,7 @@ export default function GenerateAddtionProblems() {
 
                                 <select
                                     key={''}
-                                    className={`${formInput} capitalize`}
+                                    className={`${formInput} w-auto capitalize`}
                                     required
                                     onChange={(evt) => {
                                         evt.preventDefault()
@@ -247,7 +329,7 @@ export default function GenerateAddtionProblems() {
 
                     {/* Error */}
                     {error ? (
-                        <div className="w-full flex flex-wrap mt-[10px] mb-[30px] text-error-red">
+                        <div className="w-full flex flex-wrap my-[10px] text-error-red">
                             <p className="w-full text-center">
                                 Oh no! There was an error with your request.
                             </p>
@@ -261,6 +343,7 @@ export default function GenerateAddtionProblems() {
                         key={'submit-btn'}
                         className={formBtn}
                         onClick={handleGenerateProblems}
+                        disabled={disableSubmit}
                     >
                         Generate Problems
                     </button>
