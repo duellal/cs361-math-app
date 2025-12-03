074ea28c99882ebed011b8084ae889a05aed27a4
Alex <alexandriaduell@gmail.com>
2025-12-02 19:28:38 -0500

    Added timer to tutorial

--- src/app/topics/__components/tutorial/tutorialArrays.js
+++ src/app/topics/__components/tutorial/tutorialArrays.js
@@ -1,14 +1,18 @@
 const tutorialStepsArr = [
     {
-        mainDivTw: `w-[300px] h-fit relative left-[52%] -top-[75%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[45%] -top-[62%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Click the question mark to see a video tutorial!`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[40%] -top-[80%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[35%] -top-[65%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `At any time, if you need help solving the problem, click the "Need a Hint?" button.`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[-8%] -top-[10%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[30%] -top-[78%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        text: `Click the "Start Timer" button if you want to know how long it takes you to complete a problem.`,
+    },
+    {
+        mainDivTw: `w-[300px] h-fit relative left-[-18%] -top-[0%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Move your cursor over this box and click to input your answer!`,
     },
     {
@@ -16,39 +20,39 @@ const tutorialStepsArr = [
         text: `If you don't want to answer the question, you have the option to skip the problem here.`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[46%] -top-[22%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[46%] -top-[14%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Once you input your answer, click "Submit" to see if you are right! Be careful, you can't submit until you input an answer.`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[10%] -top-[92%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[10%] -top-[82%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Once you hit submit, a pop-up will show asking you to confirm your answer.`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[-10%] -top-[32%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[-10%] -top-[45%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `You can submit your answer shown by clicking this button.`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[15%] -top-[32%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[15%] -top-[45%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Or you can go back and edit your answer.`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[30%] -top-[78%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[24%] -top-[75%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `You can even go back by clicking this X!`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[45%] -top-[68%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[44%] -top-[67%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Whether you have the correct answer, you can see the explanation for getting the right answer!`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[45%] -top-[68%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[44%] -top-[67%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `Whether you have the correct answer, you can see the explanation for getting the right answer!`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[-10%] -top-[-3%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[-10%] -top-[-8%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `If you would like to give the problem another shot, click "Try Again".`,
     },
     {
-        mainDivTw: `w-[300px] h-fit relative left-[18%] -top-[-6%] text-dark-blue text-[14px] font-medium -my-[24%]`,
+        mainDivTw: `w-[300px] h-fit relative left-[15%] -top-[-10%] text-dark-blue text-[14px] font-medium -my-[24%]`,
         text: `If you want to go to the next problem, click this button!`,
     },
 ]
@@ -71,15 +75,16 @@ export const btnArr = [
 
             evt.preventDefault()
             setNumStep(numStep - 2)
-            if (numStep - 1 === 4) {
+
+            if (numStep - 1 === 5) {
                 setSubmitDisable(true)
                 setAnswer(null)
-            } else if (numStep - 1 === 5) {
+            } else if (numStep - 1 === 6) {
                 handleConfirmClose()
-            } else if (numStep - 1 === 9) {
+            } else if (numStep - 1 === 10) {
                 setSolutionDiv(false)
                 handleConfirm()
-            } else if (numStep === 11) {
+            } else if (numStep === 12) {
                 setShowAnswer(false)
             }
         },
@@ -96,7 +101,6 @@ export const btnArr = [
                 setSubmitDisable,
                 setNumStep,
                 setSolutionDiv,
-                setBlurBg,
                 setTutorialDisable,
                 setTutorialEndDiv,
                 setShowAnswer,
@@ -104,17 +108,17 @@ export const btnArr = [
             evt.preventDefault()
             setNumStep(numStep)
 
-            if (numStep === 4) {
+            if (numStep === 5) {
                 setSubmitDisable(false)
                 setAnswer(30)
-            } else if (numStep === 5) {
+            } else if (numStep === 6) {
                 handleConfirm()
-            } else if (numStep === 9) {
+            } else if (numStep === 10) {
                 handleConfirmClose()
                 setSolutionDiv(true)
-            } else if (numStep === 10) {
+            } else if (numStep === 11) {
                 setShowAnswer(true)
-            } else if (numStep === 13) {
+            } else if (numStep === 14) {
                 setTutorialEndDiv(true)
                 setSolutionDiv(false)
                 setAnswer('')
